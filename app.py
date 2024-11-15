fa955275cd083d14a5c60ba33cccbfa757015ec6
JonathanAdriell <jonathanadriels@gmail.com>
2024-11-16 02:21:57 +0700

    Add caching for improved performance

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from PIL import Image
 LABELS = ["Katarak", "Retinopati Diabetik", "Glaukoma", "Normal"]
 
 
+@st.cache_resource
 def load_model(model_path, device):
     model = AutoModelForImageClassification.from_pretrained("google/efficientnet-b7")
     model.classifier = torch.nn.Linear(model.classifier.in_features, 4)
